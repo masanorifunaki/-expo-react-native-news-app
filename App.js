ae4b75ffe0b73b944fc6969b5d62a8933cc4c20a
舟木正憲 <funakimfunaki@gmail.com>
2019-08-28 19:55:37 +0900

    Add animation

--- App.js
+++ App.js
@@ -6,12 +6,27 @@ import {
   Image,
   Dimensions,
   ActivityIndicator,
+  Animated,
 } from 'react-native';
+
+import Spring from 'app/src/components/Spring';
 
 const App = () => {
   const [threads, setThreads] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [opacity] = useState(new Animated.Value(0));
+  const [fontSize] = useState(new Animated.Value(0));
   const { width } = Dimensions.get('window');
+  const fontLate = fontSize.interpolate({ inputRange: [0, 1], outputRange: [0, 12] });
+
+  const animate = () => {
+    Animated.timing(
+      opacity, { toValue: 1, duration: 1000 },
+    ).start();
+    Animated.spring(
+      fontSize, { toValue: 1, friction: 1 },
+    ).start();
+  };
 
   useEffect(() => {
     fetch('https://www.reddit.com/r/newsokur/hot.json')
@@ -25,6 +40,7 @@ const App = () => {
         });
         setThreads(newThreads);
         setLoading(false);
+        animate();
       })
       .catch((error) => {
         console.error(error);
@@ -38,41 +54,43 @@ const App = () => {
       }}
     >
       {isLoading
-        ? <ActivityIndicator />
+        ? <Spring />
         : (
-          <FlatList
-            data={threads}
-            renderItem={({ item }) => (
-              <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                width: '100%',
-              }}
-              >
-                <Image
-                  style={{
-                    width: 50,
-                    height: 50,
-                  }}
-                  source={{ uri: item.data.thumbnail }}
-                />
-                <View
-                  style={{ width: width - 50 }}
+          <Animated.View style={{ opacity }}>
+            <FlatList
+              data={threads}
+              renderItem={({ item }) => (
+                <View style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  width: '100%',
+                }}
                 >
+                  <Image
+                    style={{
+                      width: 50,
+                      height: 50,
+                    }}
+                    source={{ uri: item.data.thumbnail }}
+                  />
                   <View
-                    style={{ flex: 1, flexDirection: 'column' }}
+                    style={{ width: width - 50 }}
                   >
-                    <Text>{item.data.title}</Text>
-                    <Text
-                      style={{ color: '#ababab', fontSize: 10 }}
+                    <View
+                      style={{ flex: 1, flexDirection: 'column' }}
                     >
-                      {item.data.domain}
-                    </Text>
+                      <Animated.Text style={{ fontSize: fontLate }}>{item.data.title}</Animated.Text>
+                      <Text
+                        style={{ color: '#ababab', fontSize: 10 }}
+                      >
+                        {item.data.domain}
+                      </Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            )}
-          />
+              )}
+            />
+          </Animated.View>
         )}
     </View>
   );
