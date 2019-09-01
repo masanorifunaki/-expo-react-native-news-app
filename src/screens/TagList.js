75d749fb22f929dc14ed71afdf937ffed823339b
舟木正憲 <funakimfunaki@gmail.com>
2019-09-01 16:37:04 +0900

    Reset this repository for news reader app

---
+++ src/screens/TagList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Tags from 'app/src/components/Tags';
+
+class TagList extends React.Component {
+  render() {
+    return <Tags />;
+  }
+}
+
+export default TagList;
