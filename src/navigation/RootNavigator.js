96dab3cf50f93065c6fb92eaf5c24dfcd12983c6
舟木正憲 <funakimfunaki@gmail.com>
2019-09-07 10:44:26 +0900

    feat: add KeywordNavigation to RootNavigator

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -11,6 +11,8 @@ import TagListScreen from 'app/src/screens/TagListScreen';
 import TagSubscriberScreen from 'app/src/screens/TagSubscriberScreen';
 import TagPageListScreen from 'app/src/screens/TagPageListScreen';
 import WebViewPageScreen from 'app/src/screens/WebViewPageScreen';
+import KeywordListScreen from 'app/src/screens/KeywordListScreen';
+import KeywordFormScreen from 'app/src/screens/KeywordFormScreen';
 
 const TagNavigation = createStackNavigator(
   {
@@ -29,6 +31,20 @@ const TagNavigation = createStackNavigator(
   },
 );
 
+const KeywordNavigation = createStackNavigator(
+  {
+    KeywordList: {
+      screen: KeywordListScreen,
+    },
+    KeywordForm: {
+      screen: KeywordFormScreen,
+    },
+    WebViewPage: {
+      screen: WebViewPageScreen,
+    },
+  },
+);
+
 const RootNavigator = createBottomTabNavigator({
   TagNavigation: {
     screen: TagNavigation,
@@ -37,6 +53,16 @@ const RootNavigator = createBottomTabNavigator({
       tabBarLabel: 'タグ',
       tabBarIcon: ({ tintColor, focused }) => (
         <Icon name="label" />
+      ),
+    },
+  },
+  KeywordNavigation: {
+    screen: KeywordNavigation,
+    navigationOptions: {
+      title: 'Keyword',
+      tabBarLabel: 'キーワード',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon name="room" />
       ),
     },
   },
---
+++ src/screens/KeywordListScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Icon } from 'react-native-elements';
+
+import KeywordList from 'app/src/components/KeywordList';
+
+class KeywordListScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <Icon
+        color="#f50"
+        name="add"
+        iconStyle={{ marginRight: 16 }}
+        onPress={() => { navigation.navigate('KeywordForm'); }}
+      />
+    ),
+  });
+
+  render() {
+    return <KeywordList />;
+  }
+}
+
+export default KeywordListScreen;
