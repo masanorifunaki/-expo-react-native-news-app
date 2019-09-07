dbc35776c8adff2bad8eb22779b69a76beb8b271
舟木正憲 <funakimfunaki@gmail.com>
2019-09-07 10:42:04 +0900

    feat: add dispatch and action to KeywordForm

--- src/components/KeywordForm.js
+++ src/components/KeywordForm.js
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { AsyncStorage, View } from 'react-native';
 import {
   Button,
   Input,
 } from 'react-native-elements';
 
+import AppContext from 'app/src/contexts/AppContext';
+import { SAVE_KEYWORDS } from 'app/src/actions';
+
 const KeywordForm = () => {
+  const { dispatch } = useContext(AppContext);
   const [keyword, setKeyword] = useState('');
 
   const registerKeyword = async () => {
     try {
       const Keywords = await AsyncStorage.getItem('Keywords', null);
       let keywords = JSON.parse(Keywords);
-
-      if (keywords === null) {
-        keywords = [];
-      }
+      const oldLength = keywords.length;
 
       if (typeof keywords === 'object' && keywords.length >= 0 && keyword) {
         keywords = keywords.concat(keyword);
         keywords = [...new Map(keywords.map((k) => [k, k])).values()];
+        const newLength = keywords.length;
         await AsyncStorage.setItem('Keywords', JSON.stringify(keywords), null);
+
+        if (oldLength !== newLength) {
+          await dispatch({
+            type: SAVE_KEYWORDS,
+            savedKeywords: keywords,
+          });
+        }
       }
     } catch (e) {
       console.error(e);
