f4ad26b865929cb9b8e3e2eb5988d39425d7eb95
舟木正憲 <funakimfunaki@gmail.com>
2019-09-07 10:42:54 +0900

    feat: add KeywordList to components

---
+++ src/components/KeywordList.js
@@ -0,0 +1,58 @@
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+} from 'react';
+import {
+  FlatList,
+  AsyncStorage,
+  ActivityIndicator,
+} from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+import AppContext from 'app/src/contexts/AppContext';
+
+const KeywordList = () => {
+  const { state } = useContext(AppContext);
+  const [isLoading, setLoading] = useState(true);
+
+  const loadKeywords = useCallback(async () => {
+    try {
+      const Keywords = await AsyncStorage.getItem('Keywords', null);
+      const savedKeywords = JSON.parse(Keywords);
+      if (typeof savedKeywords === 'object' && savedKeywords.length >= 0) {
+        state.savedKeywords = savedKeywords;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, [state.savedKeywords]);
+
+  useEffect(() => {
+    const callback = async () => {
+      await loadKeywords();
+      await setLoading(false);
+    };
+    callback();
+  }, [loadKeywords]);
+
+  if (isLoading && state.savedKeywords.length === 0) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <FlatList
+      data={state.savedKeywords}
+      renderItem={({ item }) => (
+        <ListItem
+          key={item}
+          title={item}
+        />
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
+  );
+};
+
+export default KeywordList;
