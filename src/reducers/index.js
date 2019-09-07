9f8d8fe089d2a9ac500ff793e8a2389ce031f0f6
舟木正憲 <funakimfunaki@gmail.com>
2019-09-07 10:40:43 +0900

    feat: add them to use new savedKeywords for initial values of state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import savedTags from 'app/src/reducers/savedTags';
+import savedKeywords from 'app/src/reducers/savedKeywords';
 
 export default combineReducers({
   savedTags,
+  savedKeywords,
 });
